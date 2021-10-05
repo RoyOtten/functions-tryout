d15710c9147fa69fb93c1a88b0cb446e8974da4f
RoyOtten <99065498@mydavinci.nl>
2021-10-05 12:04:49 +0200

    derde commit

---
+++ calculations.py
@@ -0,0 +1,35 @@
+
+def addition1():
+    addition1 = int(input ("number1"))
+    addition2 = int(input ("number2")) 
+    additions = (int(addition1 + addition2)) 
+    print (str(addition1),  " + ", str(addition2), " = ", str(additions))
+
+ 
+    
+def addition2():
+    addition1 = int(input ("number1"))
+    addition2 = int(input ("number2")) 
+    additions = (int(addition1 - addition2)) 
+    print (str(addition1),  " - ", str(addition2), " = ", str(additions))
+
+
+
+def addition3():
+    addition1 = int(input ("number1"))
+    addition2 = int(input ("number2")) 
+    additions = (int(addition1 * addition2)) 
+    print (str(addition1),  " * ", str(addition2), " = ", str(additions))
+
+
+
+def addition4():
+    addition1 = int(input ("number1"))
+    addition2 = int(input ("number2")) 
+    additions = (int(addition1 / addition2)) 
+    print (str(addition1),  " : ", str(addition2), " = ", str(additions))
+
+addition1()
+addition2()
+addition3() 
+addition4()

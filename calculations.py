add435798e8ff634a3c231184cc4112bf0b8ac57
RoyOtten <99065498@mydavinci.nl>
2021-10-05 12:16:00 +0200

    eerste commit

--- calculations.py
+++ calculations.py
@@ -26,7 +26,7 @@ def addition3():
 def addition4():
     addition1 = int(input ("number1"))
     addition2 = int(input ("number2")) 
-    additions = (int(addition1 / addition2)) 
+    additions = addition1 // addition2
     print (str(addition1),  " : ", str(addition2), " = ", str(additions))
 
 addition1()
---
+++ leeftijdnaam.py
@@ -0,0 +1,18 @@
+def automatic(naam , leeftijd):
+    print('hello' + naam + 'je leeftijd is ' + leeftijd) 
+
+a = 0 
+while a <=1:
+    input1 = input("vul je naam in") 
+    if input == 'stop':
+        break
+    input2 = input("vul je leeftijd in") 
+    if input2 == 'stop':
+        break
+
+    ognaam = input1
+    ogleeftijd = input2 
+
+automatic(ognaam,ogleeftijd) 
+
+    
